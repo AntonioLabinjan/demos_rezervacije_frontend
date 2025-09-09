0979c5e4ad0ac2ab09e9a59aa391a95708b7256f
Antonio Labinjan <alabinjan6@gmail.com>
2025-09-09 20:37:29 +0200

    Porko nebo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <main class="min-h-screen bg-gray-100 py-10">
-    <h1 class="text-3xl text-center font-bold mb-6">Rezervacija termina za demonstrature</h1>
     <ReservationForm />
-  </main>
 </template>
 
 <script setup>
